27ba93add68145e7d291fbb4bc1cb55408a8b3ae
Rzesiu <250994@edu.p.lodz.pl>
2024-06-21 14:22:08 +0200

    added functions

---
+++ text_chain.h
@@ -0,0 +1,15 @@
+
+
+#ifndef TEXT_CHAIN_H
+#define TEXT_CHAIN_H
+
+#define SIZE 10
+
+void  enterValue(int *array);
+void printArray(int *array);
+int findMaximumValue(int *array);
+int findMiniumValue(int *array);
+float calculateAverage(int *array);
+void saveValue(int array[]);
+void openValues(int array[]);
+#endif //TEXT_CHAIN_H
